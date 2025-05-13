9b0fc1cb812201ac4201db8309303e8057f794a6
jettpbaker <jett.p.baker@gmail.com>
2025-05-13 19:09:22 +1000

    refactored how ffmpeg instantiation and accessing works

---
+++ src/services/ffmpeg.js
@@ -0,0 +1,57 @@
+import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { ref } from 'vue'
+import { toBlobURL } from '@ffmpeg/util'
+
+const instance = new FFmpeg()
+const isReady = ref(false)
+const isLoading = ref(false)
+const message = ref(null)
+let loadPromise = null
+
+instance.on('log', ({ ffmpegMessage }) => {
+  console.log('[FFMPEG Log]:', ffmpegMessage)
+  message.value = ffmpegMessage
+})
+
+const ffmpeg = {
+  isReady,
+  isLoading,
+  message,
+  load: async () => {
+    if (isReady.value) {
+      return
+    }
+    if (isLoading.value && loadPromise) {
+      return loadPromise
+    }
+
+    isLoading.value = true
+    isReady.value = false
+    message.value = 'Loading FFMPEG core'
+
+    loadPromise = (async () => {
+      try {
+        const baseURL = 'https://cdn.jsdelivr.net/npm/@ffmpeg/core-mt@0.12.9/dist/esm'
+        const corePath = `${baseURL}/ffmpeg-core.js`
+        const wasmPath = `${baseURL}/ffmpeg-core.wasm`
+        const workerPath = `${baseURL}/ffmpeg-core.worker.js`
+
+        await instance.load({
+          coreURL: await toBlobURL(corePath, 'text/javascript'),
+          wasmURL: await toBlobURL(wasmPath, 'application/wasm'),
+          workerURL: await toBlobURL(workerPath, 'text/javascript'),
+        })
+
+        isReady.value = true
+      } catch (err) {
+        console.error(err)
+        isReady.value = false
+      } finally {
+        isLoading.value = false
+      }
+    })()
+    return loadPromise
+  },
+}
+
+export default ffmpeg
