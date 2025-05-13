5f67dce48446b38ab7337033a4535fc1ec7e1c3a
jettpbaker <jett.p.baker@gmail.com>
2025-05-13 14:47:54 +1000

    basic layout setup done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
-<script setup></script>
+<script setup>
+import HeaderContainer from './components/layout/HeaderContainer.vue'
+import ContentContainer from './components/layout/ContentContainer.vue'
+</script>
 
 <template>
-  <h1>Hello World</h1>
-  <v-card>
-    <v-card-title> This app is being styled with Vuetify </v-card-title>
-    <v-card-text> This is a card component from Vuetify </v-card-text>
-    <v-card-actions>
-      <v-btn variant="tonal">Click me</v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-app>
+    <HeaderContainer />
+    <ContentContainer />
+  </v-app>
 </template>
---
+++ src/components/layout/MainContainer.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <v-col cols="12" sm="8" class="pa-5 order-1" style="min-height: 50vh">
+    <h1>Main</h1>
+  </v-col>
+</template>
